218baa76e80b369f0d898abddbc76aa0134ec013
sosuke-k <snoopies.drum@gmail.com>
2015-11-25 01:26:10 +0900

    refactor data/main.go, data/README.md and related files...

--- data/README.md
+++ data/README.md
@@ -2,7 +2,7 @@
 
 Twitter free data for Japanese task of [NTCIR-12 STC](http://ntcir12.noahlab.com.hk/stc.htm) includes only `twitter_id`, so we have to fetch it without money.
 
-I used [GORM](https://github.com/jinzhu/gorm), MySQL, [cron](https://github.com/robfig/cron) and [twittergo](https://github.com/kurrik/twittergo) and referred to [twittergo-examples](https://github.com/kurrik/twittergo-examples).
+I used [GORM](https://github.com/jinzhu/gorm), MySQL and [goquery](https://github.com/PuerkitoBio/goquery).
 
 ## preliminary
 
@@ -13,23 +13,6 @@ wget https://github.com/mynlp/stc/raw/master/taskdata/twitter_id_str_data.txt.bz
 bzip2 -d twitter_id_str_data.txt.bz2
 ```
 
-### Twitter API
-
-```
-touch CREDENTIALS
-```
-
-please edit CREDENTIALS
-
-CREDENTIALS sample:
-
-```
-<Twitter consumer key>
-<Twitter consumer secret>
-<Twitter access token>
-<Twitter access token secret>
-```
-
 ### MySQL
 
 ```
@@ -38,55 +21,45 @@ mysql> CREATE DATABASE trm DEFAULT CHARACTER SET utf8;
 mysql> GRANT ALL PRIVILEGES ON trm.* TO trm@localhost;
 ```
 
-If you change username, databasename and password, edit `trm/database.go`.
+If you change username, databasename and password, edit `twitter/database.go`.
 
 ### Dependencies
 
 please `go get`
 
-* github.com/codingneo/twittergo
+* github.com/PuerkitoBio/goquery
 * github.com/go-sql-driver/mysql
 * github.com/jinzhu/gorm
-* github.com/kurrik/oauth1a
-* github.com/robfig/cron
+* github.comjoho/godotenv
 
 ## using
 
 ```
-go run main.go -reset true
+go run main.go -reset
 ```
 
 ### Usage
 
 ```
-go run main.go [-start INT] [-reset BOOL]
-options:
-  start : starting line of dataset
-  reset : reset tables of database
-```
-
-### Cron
-
-There is [API Rate Limits](https://dev.twitter.com/rest/public/rate-limiting) of Twitter API Requests.
-I call function every 17 minutes, so many many many long hours......
-
-First, I try every 15 minutes, but this
-
-```
-2015/11/12 13:01:41 Rate limited, reset at 2015-11-12 13:15:00 +0900 JST
-2015/11/12 13:01:41 Next, start at line index 90
-2015/11/12 13:15:00 index of lines is 90
-2015/11/12 13:15:00 Rate limited, reset at 2015-11-12 13:15:00 +0900 JST
-2015/11/12 13:15:00 Next, start at line index 90
+go run main.go [-start INT] [-reset] [-check] [-slack STRING]
+options(default):
+  start(0)     : starting line of dataset
+  reset(false) : reset tables of database needed first run
+  check(false) : duplication id check of tweet at start line
+  slack("")    : channel of slack if notification needed
 ```
 
 ### Log
 
 Please, look at `trm.log`.
 
-### Notification Network Error
+### Notification Finish
 
-Please set `INCOMMING_URL` of environment variable with ["github.comjoho/godotenv"](https://github.com/joho/godotenv)
+You can be notified when finished via slack channel.
+e.g. `go run main.go -slack general`
+
+Please set `INCOMMING_URL` of environment variable with ["github.comjoho/godotenv"](https://github.com/joho/godotenv).
+So, put `.env` file.
 
 .env sample:
 
@@ -94,62 +67,12 @@ Please set `INCOMMING_URL` of environment variable with ["github.comjoho/godoten
 INCOMMING_URL="https://hooks.slack.com/services/..."
 ```
 
-## structure
-
-```
-mysql> use trm
-Reading table information for completion of table and column names
-You can turn off this feature to get a quicker startup with -A
-
-Database changed
-mysql> SHOW TABLES;
-+---------------+
-| Tables_in_trm |
-+---------------+
-| conversations |
-| tweets        |
-| users         |
-+---------------+
-3 rows in set (0.00 sec)
-
-mysql> DESC users;
-+----------+--------------+------+-----+---------+----------------+
-| Field    | Type         | Null | Key | Default | Extra          |
-+----------+--------------+------+-----+---------+----------------+
-| id       | int(11)      | NO   | PRI | NULL    | auto_increment |
-| name     | varchar(255) | NO   |     | NULL    |                |
-| nickname | varchar(255) | NO   | UNI | NULL    |                |
-+----------+--------------+------+-----+---------+----------------+
-3 rows in set (0.00 sec)
-
-mysql> DESC tweets;
-+------------+--------------+------+-----+---------+----------------+
-| Field      | Type         | Null | Key | Default | Extra          |
-+------------+--------------+------+-----+---------+----------------+
-| id         | int(11)      | NO   | PRI | NULL    | auto_increment |
-| twitter_id | varchar(255) | NO   | UNI | NULL    |                |
-| success    | int(11)      | NO   |     | NULL    |                |
-| user_id    | int(11)      | YES  | MUL | NULL    |                |
-| text       | varchar(255) | YES  |     | NULL    |                |
-| created_at | timestamp    | YES  |     | NULL    |                |
-+------------+--------------+------+-----+---------+----------------+
-6 rows in set (0.01 sec)
-
-mysql> DESC conversations;
-+-----------------+---------+------+-----+---------+----------------+
-| Field           | Type    | Null | Key | Default | Extra          |
-+-----------------+---------+------+-----+---------+----------------+
-| id              | int(11) | NO   | PRI | NULL    | auto_increment |
-| first_tweet_id  | int(11) | NO   | MUL | NULL    |                |
-| second_tweet_id | int(11) | NO   | MUL | NULL    |                |
-+-----------------+---------+------+-----+---------+----------------+
-3 rows in set (0.00 sec)
-```
-
-![https://gyazo.com/57d73011d2c9135f8a13f7e5759dbef6](https://i.gyazo.com/57d73011d2c9135f8a13f7e5759dbef6.png)
-
 ## notice
 
-### Importing local package
+There are packages under subfolders, but not buildable at root.
 
-Do not put this repository under `GOPATH` folder because of importing local package.
+So, please this:
+
+```
+go get -d github.com/sosuke-k/twitter-responding-machine
+```
--- data/main.go
+++ data/main.go
@@ -7,78 +7,14 @@ import (
 	"os"
 	"strings"
 
-	"github.com/sosuke-k/twitter-responding-machine/data/trm/twitter"
+	"github.com/joho/godotenv"
+	"github.com/sosuke-k/twitter-responding-machine/data/logger"
+	"github.com/sosuke-k/twitter-responding-machine/data/slack"
+	"github.com/sosuke-k/twitter-responding-machine/data/twitter"
 )
 
-var reset *bool
-var update *bool
-var start *int
-var startIdx int
-
-// EverySeventeen called per 17 minutes
-// func EverySeventeen() {
-// 	logger := GetLogger()
-//
-// 	var (
-// 		err    error
-// 		db     gorm.DB
-// 		client *twittergo.Client
-// 	)
-//
-// 	db, err = DB()
-// 	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "Could not open database: %v\n", err)
-// 		os.Exit(1)
-// 	}
-// 	defer db.Close()
-//
-// 	client, err = LoadCredentials()
-// 	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "Could not parse CREDENTIALS file: %v\n", err)
-// 		os.Exit(1)
-// 	}
-//
-// 	data, err := ioutil.ReadFile("twitter_id_str_data.txt")
-// 	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "Could not read twitter_id_str_data.txt: %v\n", err)
-// 		os.Exit(1)
-// 	}
-// 	lines := strings.Split(string(data), "\n")
-// 	for i := startIdx; i < len(lines); i++ {
-// 		logger.Printf("index of lines is %d\n", i)
-// 		line := lines[i]
-// 		ids := strings.Split(line, "\t")
-// 		firstID, err := SaveTweet(&db, client, ids[0], *update)
-// 		if err != nil {
-// 			if err.(*TwitterError).Op == OpLimit {
-// 				startIdx = i
-// 				logger.Printf("Next, start at line index %d\n", startIdx)
-// 				return
-// 			}
-// 			panic(err)
-// 		}
-// 		secondID, err := SaveTweet(&db, client, ids[1], *update)
-// 		if err != nil {
-// 			if err.(*TwitterError).Op == OpLimit {
-// 				startIdx = i
-// 				logger.Printf("Next, start at line index %d\n", startIdx)
-// 				return
-// 			}
-// 			panic(err)
-// 		}
-// 		conversation := Conversation{
-// 			FirstTweetID:  firstID,
-// 			SecondTweetID: secondID,
-// 		}
-// 		err = db.Create(&conversation).Error
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		logger.Println("insert conversation(" + ids[0] + ", " + ids[1] + ")")
-// 	}
-// }
-
 func duplicationCheck(start int) {
+
 	data, err := ioutil.ReadFile("twitter_id_str_data.txt")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not read twitter_id_str_data.txt: %v\n", err)
@@ -90,91 +26,98 @@ func duplicationCheck(start int) {
 
 	db, err := twitter.DB()
 	if err != nil {
-		fmt.Println("db")
-		fmt.Println(err.Error())
+		fmt.Fprintf(os.Stderr, "Could not open database: %v\n", err)
+		os.Exit(1)
 	}
 	defer db.Close()
 
 	var tweet twitter.Tweet
 	if db.Where("item_id = ?", itemID).First(&tweet).RecordNotFound() {
-		fmt.Println("tweet(id:" + itemID + ") not exists")
+		fmt.Fprintf(os.Stdout, "tweet(id:%s) not exists\n", itemID)
 		fmt.Println("this start index is ok")
 	} else {
-		fmt.Println("tweet(id:" + itemID + ") exists")
+		fmt.Fprintf(os.Stdout, "tweet(id:%s) exists\n", itemID)
+		fmt.Println("this start index is not good")
 	}
 
 }
 
-func scrape(start int) {
+func gather(start int) {
+	logger := logger.GetInstance()
+
 	db, err := twitter.DB()
 	if err != nil {
-		fmt.Println("db")
-		fmt.Println(err.Error())
+		fmt.Fprintf(os.Stderr, "Could not open database: %v\n", err)
+		os.Exit(1)
 	}
 	defer db.Close()
 
 	data, err := ioutil.ReadFile("twitter_id_str_data.txt")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not read twitter_id_str_data.txt: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Could not open database: %v\n", err)
 		os.Exit(1)
 	}
 	lines := strings.Split(string(data), "\n")
 	for i := start; i < len(lines); i++ {
 		fmt.Printf("index of lines is %d\n", i)
+		logger.Printf("index of lines is %d\n", i)
 		line := lines[i]
 		ids := strings.Split(line, "\t")
 		tweet := twitter.Tweet{ItemID: ids[0]}
 		err := tweet.Fetch()
 		if err != nil {
-			fmt.Println("fetch")
-			fmt.Println(err.Error())
+			logger.Println("Could not fetch tweet:")
+			logger.Printf("    item is = %s\n", tweet.ItemID)
+			logger.Fatalln(err)
+			fmt.Fprintf(os.Stderr, "Could not fetch tweet: %v\n", err)
 		}
 		err = tweet.Save(&db)
 		if err != nil {
-			fmt.Println("save")
-			fmt.Println(err.Error())
+			logger.Println("Could not save tweet:")
+			logger.Printf("    item is = %s\n", tweet.ItemID)
+			logger.Fatalln(err)
+			fmt.Fprintf(os.Stderr, "Could not save tweet: %v\n", err)
 		}
+		logger.Println("Successed inserting tweet:")
+		logger.Printf("    item is = %s\n", tweet.ItemID)
 	}
 }
 
 func main() {
-	// logger := GetLogger()
+	logger := logger.GetInstance()
 
 	check := flag.Bool("check", false, "duplication id check of tweet at start line")
 	reset := flag.Bool("reset", false, "reset database")
-	// update := flag.Bool("update", false, "update record")
 	start := flag.Int("start", 0, "start index")
+	channel := flag.String("slack", "", "channel of slack if notification needed")
 	flag.Parse()
+
+	fmt.Fprintf(os.Stdout, "starting line number = %d\n", *start)
 
 	if *check {
 		duplicationCheck(*start)
 		return
 	}
 
-	if *reset {
-		fmt.Fprintln(os.Stdout, "reset tables...")
-		// logger.Println("reset tables...")
-		twitter.Reset()
-		fmt.Fprintln(os.Stdout, "done")
-		// logger.Println("done")
+	if *channel != "" {
+		if err := godotenv.Load(".env"); err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
 	}
 
-	scrape(*start)
-	// startIdx = *start
+	if *reset {
+		fmt.Fprintln(os.Stdout, "reset tables...")
+		logger.Println("reset tables...")
+		twitter.Reset()
+		fmt.Fprintln(os.Stdout, "done")
+		logger.Println("done")
+	}
 
-	// if err := godotenv.Load("../slack/.env"); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "%v\n", err)
-	// }
-	//
-	// fmt.Fprintf(os.Stdout, "starting at %d ...\n", *start)
-	//
-	// c := cron.New()
-	// c.AddFunc("@every 17m", EverySeventeen)
-	// c.Start()
-	//
-	// for {
-	// 	time.Sleep(10000000000000)
-	// 	fmt.Println("still sleeping...")
-	// }
+	gather(*start)
+
+	if *channel != "" {
+		slack.Post(*channel, "finished gathering!")
+	}
 
 }
